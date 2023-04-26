ab60ae346cd33ce65f4ca93eeb9aa1355e0edd30
Paulo Costa <pjrc83@gmail.com>
2023-04-26 17:44:43 -0300

    Classe Model reserva

---
+++ src/modelo/Reserva.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Reserva {
+
+	private Integer id;
+	
+	private Date dataEntreda;
+	
+	private Date dataSaida;
+	
+	private String valor;
+	
+	private String formaPagamento;
+
+}
