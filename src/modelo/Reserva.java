ac1e2ab40354c36b43f7e448d149505106f04f3e
Paulo Costa <pjrc83@gmail.com>
2023-04-27 16:54:03 -0300

    Alteração na classe modelo reserva

--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -1,17 +1,66 @@
 package modelo;
 
 import java.sql.Date;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class Reserva {
 
 	private Integer id;
 	
-	private Date dataEntreda;
+	private String dataEntrada;
 	
-	private Date dataSaida;
+	private String dataSaida;
 	
-	private String valor;
+	private Double valor;
 	
-	private String formaPagamento;
+	private String formaDePagamento;
 
+	public Reserva(String dataEntrada, String dataSaida, Double valor, String formaDePagamento) {
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+		this.valor = valor;
+		this.formaDePagamento = formaDePagamento;
+	}
+
+	public Reserva(Integer id, String dataEntrada, String dataSaida, Double valor, String formaDePagamento) {
+		this(dataEntrada, dataSaida, valor, formaDePagamento);
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDataEntrada() {
+		return this.dataEntrada;
+	}
+
+	public String getDataSaida() {
+		return this.dataSaida;
+	}
+
+	public Double getValor() {
+		return this.valor;
+	}
+
+	public String valorFormatado() {
+		Locale brasil = new Locale("pt", "BR");
+		return NumberFormat.getCurrencyInstance(brasil).format(this.valor);
+	}
+
+	public String getFormaDePagamento() {
+		return this.formaDePagamento;
+	}
+
+	public static double valor(String dataEntrada, String dataSaida) {
+		long dias = ChronoUnit.DAYS.between(LocalDate.parse(dataEntrada), LocalDate.parse(dataSaida)) + 1;
+		return dias * 150;
+	}
 }
