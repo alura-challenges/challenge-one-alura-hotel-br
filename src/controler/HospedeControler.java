5b58792da034f4c4701f65401ca155366f4f52b7
Paulojoserc <pjrc83@gmail.com>
2023-04-30 00:21:08 -0300

    Criação da casse HospedeControler

--- src/controler/HospedeControler.java
+++ src/controler/HospedeControler.java
@@ -1,5 +1,37 @@
 package controler;
 
-public class HospedeControler {
+import java.sql.Connection;
+import java.util.List;
 
+import dao.HospedeDAO;
+import factory.ConnectionFactory;
+import modelo.Hospede;
+
+public class HospedeControler {
+	private HospedeDAO hospedeDAO;
+
+	public HospedeControler() {
+		Connection connection = new ConnectionFactory().recuperarConexao();
+		this.hospedeDAO = new HospedeDAO(connection);
+	}
+
+	public void salvar(Hospede hospede) {
+		this.hospedeDAO.salvar(hospede);
+	}
+
+	public List<Hospede> listar() {
+		return this.hospedeDAO.listar();
+	}
+
+	public void deletar(Integer id) {
+		this.hospedeDAO.deletar(id);
+	}
+
+	public void alterar(Hospede hospede) {
+		this.hospedeDAO.alterar(hospede);
+	}
+
+	public List<Hospede> pesquisar(String pesquisa) {
+		return this.hospedeDAO.pesquisar(pesquisa);
+	}
 }
